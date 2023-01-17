a163daface0dd1959efa106b5db13aba8434be79
Den Jo <nilotic2@gmail.com>
2023-01-17 21:10:55 +0900

    Create the Kurly Project

---
+++ Kurly/KurlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KurlyApp.swift
+//  Kurly
+//
+//  Created by Den Jo on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct KurlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
