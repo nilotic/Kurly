3e784534e9520bde91681f3c2403cf0ca38bd83a
Den Jo <nilotic2@gmail.com>
2023-01-18 10:05:27 +0900

    [Issue4] Add UserDefaults Keys

---
+++ Kurly/Util/UserDefault/UserDefaultsKey.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultskey.swift
+//  Kurly
+//
+//  Created by Den Jo on 2023/01/17.
+//
+
+import Foundation
+
+struct UserDefaultsKey {
+    static let suiteName   = "group.kurly.com"
+    static let kurlyServer = "kurlyServer"
+}
