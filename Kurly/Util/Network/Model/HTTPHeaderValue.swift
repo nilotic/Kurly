0ec3270bb337dc17a4fe8085b76b7f11fe5ff497
Den Jo <nilotic2@gmail.com>
2023-01-19 12:20:31 +0900

    [Issue6] Update HTTP Header Values

--- Kurly/Util/Network/Model/HTTPHeaderValue.swift
+++ Kurly/Util/Network/Model/HTTPHeaderValue.swift
@@ -8,11 +8,8 @@
 import Foundation
 
 enum HTTPHeaderValue {
-    case applicationJSON
-    case urlEncoded
     case utf8
-    case utf8Endcoded
-    case applicationJsonUTF8
+    case application(HTTPHeaderApplicationType)
     case multipart(MultipartType)
 }
 
@@ -20,12 +17,9 @@ extension HTTPHeaderValue {
     
     var rawValue: String {
         switch self {
-        case .applicationJSON:              return "application/json"
-        case .urlEncoded:                   return "application/x-www-form-urlencoded"
         case .utf8:                         return "charset=utf-8"
-        case .utf8Endcoded:                 return "application/x-www-form-urlencoded;charset=utf-8"
-        case .applicationJsonUTF8:          return "application/json;charset=utf-8"
-        case .multipart(let multipart):     return "multipart/\(multipart.rawValue)"
+        case .application(let type):        return "application/\(type.rawValue)"
+        case .multipart(let type):          return "multipart/\(type.rawValue)"
         }
     }
 }
