0ab329cd397f67d3a52a413a4c518fc8cef7f901
Den Jo <nilotic2@gmail.com>
2023-01-19 13:17:54 +0900

    [Issue9] Add frame function in the ViewExtension

--- Kurly/Component/View/ViewExtension.swift
+++ Kurly/Component/View/ViewExtension.swift
@@ -9,6 +9,18 @@ import SwiftUI
 
 extension View {
     
+    func frame(perform: @escaping (CGRect) -> Void) -> some View {
+        background(
+            GeometryReader {
+                Color.clear
+                    .preference(key: FramePreferenceKey.self, value: $0.frame(in: .global))
+            }
+        )
+        .onPreferenceChange(FramePreferenceKey.self) { value in
+            DispatchQueue.main.async { perform(value) }
+        }
+    }
+    
     func toast(message: Binding<String>) -> some View {
         modifier(ToastModifier(message: message))
     }
